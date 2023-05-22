757ae6c83af3f3b5c6dd8c702ccd0ab945a63976
RAHUL <rahulmeetawa@gmail.com>
2023-05-22 17:36:36 +0530

    add all file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { useSelector } from "react-redux";
-const App = () => {
-  const curState = useSelector((state) => state.number);
-  return (
-    <>
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <h1>Increament/Decrement Counter</h1>
-        <h1>{curState}</h1>
-        <div>
-          <button>DEC</button>
-          <button>INC</button>
-        </div>
-      </div>
-    </>
-  );
-};
+import { Counter } from "./features/counter/counter";
+import "./App.css";
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter />
+      </header>
+    </div>
+  );
+}
 export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,9 @@
+import counterReducer from "../features/counter/counterSlice";
+
 import { configureStore } from "@reduxjs/toolkit";
+
 export default configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });

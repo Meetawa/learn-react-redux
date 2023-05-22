8ea4bfe49d6dad1ba9df094b9c1ade123fce61c2
RAHUL <rahulmeetawa@gmail.com>
2023-05-22 11:17:22 +0530

    remove

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Increament and Decrement Counter</h1>
-    </div>
+    <>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Increament/Decrement Counter</h1>
+        <h1>0</h1>
+        <div>
+          <button>DEC</button>
+          <button>INC</button>
+        </div>
+      </div>
+    </>
   );
 };
 
